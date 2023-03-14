763b03735553f81163aefd1113cffb4681f374a2
luthfikumara0 <127177792+luthfikumara0@users.noreply.github.com>
2023-03-14 10:18:48 +0700

    mengumpulkan tugas

---
+++ Modul_01/Exercise-01.py
@@ -0,0 +1,12 @@
+#Exercise 01
+Nama = "Luthfi Kumara jati"
+NIM = "I0322071"
+Visualisasi = "Saya berasal dari Jogja tepatnya di Kabupaten Kulon Progo, tempat tinggal saya termasuk pegunungan menoreh sehingga suasana lingkungan sekitar tempat tinggal saya masih asri"
+Harapan = "Harapan saya setelah menjadi lulusan TI adalah ingin bekerja di perusahaan BUMN"
+
+
+print("Hello semua")
+print("Perkenalkan namaku",Nama)
+print("Dengan NIM",NIM)
+print(Visualisasi)
+print(Harapan)
